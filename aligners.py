9ee609b85d7a16adc65f58f64eb26ea81af75537
balodhi <balodhi@gmail.com>
2018-01-09 15:06:44 +0900

    Update aligners.py

--- aligners.py
+++ aligners.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 '''
 aligner class the base parent class to implement function on basic lcass levels
 while each aligner is a child of the aligner class
@@ -6,6 +7,8 @@ while each aligner is a child of the aligner class
 
 class aligner:
     """Class to represent an aligner"""
+    #def __init__(self,inputfile=None,outputfile=None):
+    	
     @property
     def num_examples(self):
         """Return qtty of examples in dataset"""
@@ -16,32 +19,39 @@ class aligner:
         raise NotImplementedError
  
 class hisat(aligner):
- 	#def __init__(self):
- 		#inial
- 	
- 	def build_command(self,genome):
- 		cmd = "hisat-build ../data/%s %s" %(genome,genome)
- 		
- 		return cmd
-
-
+    def __init__(self,inputfile,outputfile=None,workingpath=None):
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+        self.workingpath = workingpath
+     #   self.workingpath=21
+    @property
+    def build_command(self):
+        filename = os.path.join(self.workingpath, self.inputfile)
+        name, extension = os.path.splitext(self.inputfile)
+        cmd = ["hisat-build",filename,name] 
+        return cmd
 
 class hisat2(aligner):
- 	#def __init__(self):
+    def __init__(self,inputfile,outputfile=None,workingpath=None):
+        self.inputfile = inputfile
+        self.outputfile = outputfile
+        self.workingpath = workingpath
  		#inial
  	
- 	def build_command(self,genome):
- 		cmd = "hisat2-build ../data/%s.fa %s" %(genome,genome)
- 		
- 		return cmd
+    @property
+    def build_command(self):
+        filename = os.path.join(self.workingpath, self.inputfile)
+        name, extension = os.path.splitext(self.inputfile)
+        cmd = ["hisat-build2",filename,name]
+        return cmd
 
 
 class bowtie(aligner):
  	#def __init__(self):
  		#inial
  	
- 	def build_command(self,genome):
- 		cmd = "bowtie-build ../data/%s %s" %(genome,genome)
+ 	def build_command(self,inputfile):
+ 		cmd = "bowtie-build ../data/%s %s" %(inputfile,inputfile)
 
 
 class star(aligner):
@@ -50,6 +60,7 @@ class star(aligner):
  	
  	def build_command(self,genome):
  		cmd = "../../aligners/bin/STAR --runMode genomeGenerate --genomeDir . --genomeFastaFiles ../../data/%s.fa" % (genome)
+
 class gsnap(aligner):
  	#def __init__(self):
  		#inial
